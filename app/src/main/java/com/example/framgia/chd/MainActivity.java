9834261b94937154ef69614288e0a246b7b3d1e0
framgia.nguyen.van.quang <framgia@framgia0147-lt.framgia.com>
2016-11-30 14:30:56 +0700

    some change

--- app/src/main/java/com/example/framgia/chd/MainActivity.java
+++ app/src/main/java/com/example/framgia/chd/MainActivity.java
@@ -1,12 +1,34 @@
 package com.example.framgia.chd;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    private Spinner mGenderSpinner;
+    private EditText mEdtName, mEdtAge;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        mEdtName = (EditText) findViewById(R.id.input_name);
+        mEdtAge = (EditText) findViewById(R.id.input_age);
+        // set up gender spinner
+        String[] genders = {"Male", "Female"};
+        mGenderSpinner = (Spinner) findViewById(R.id.spinner_gender);
+        ArrayAdapter<String> genderAdapter = new ArrayAdapter<>
+            (
+                this,
+                android.R.layout.simple_spinner_item,
+                genders
+            );
+        mGenderSpinner.setAdapter(genderAdapter);
     }
 }
